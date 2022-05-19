992bf7223fa48da7714403c0656e27d8cd59ae20
Connor-Colenso <52056774+Connor-Colenso@users.noreply.github.com>
2022-05-19 16:05:14 +0100

    Update Course.h

--- Marking/Course.h
+++ Marking/Course.h
@@ -1,55 +1,54 @@
 #pragma once
+#include "CourseResult.h"
 #include <string>
 #include <iostream>
 #include <map>
-#include <string>
-#include <nlohmann/json.hpp>
+#include <functional>
 #include <regex>
 #include <fstream>
 #include <numeric>
-#include <functional>
-#include "CourseResult.h"
+#include <nlohmann/json.hpp>
 
 const enum Credits {
-	TenCredits = 10, TwentyCredits = 20
+    TenCredits = 10, TwentyCredits = 20
 };
 
 // Abstract base class.
 const class Course {
 protected:
-	// Variables.
-	std::string identifier;
-	std::vector<double> weights;
-	Credits numberOfCredits{};
+    // Variables.
+    std::string identifier;
+    std::vector<double> weights;
+    Credits numberOfCredits{};
 public:
-	// Constructor.
-	const Course() = default;
-	// You said you won't mark us down for this error.
-	NLOHMANN_DEFINE_TYPE_INTRUSIVE(Course, identifier, weights, numberOfCredits);
-	
-	// Virtual methods.
-	virtual const CourseResult getGrade(const std::vector<double>& grades) const = 0;
-	
-	// Methods.
-	const std::vector<double> getWeights() const;
-	const Credits getNumberOfCredits() const;
-	const std::string getIdentifier() const;
-	const std::smatch validateIdentifer() const;
-	const bool validateWeights() const;
+    // Constructor.
+    const Course() = default;
+    // You said you won't mark us down for this error.
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(Course, identifier, weights, numberOfCredits);
+
+    // Virtual methods.
+    virtual const CourseResult getGrade(const std::vector<double>& grades) const = 0;
+
+    // Methods.
+    const std::vector<double> getWeights() const;
+    const Credits getNumberOfCredits() const;
+    const std::string getIdentifier() const;
+    const std::smatch validateIdentifer() const;
+    const bool validateWeights() const;
 };
 
 // Callable classes.
 const class ExamOnly : public Course {
 public:
-	const CourseResult getGrade(const std::vector<double>& grades) const;
+    const CourseResult getGrade(const std::vector<double>& grades) const;
 };
 
 const class CourseworkOnly : public Course {
 public:
-	const CourseResult getGrade(const std::vector<double>& grades) const;
+    const CourseResult getGrade(const std::vector<double>& grades) const;
 };
 
 const class Hybrid : public Course {
 public:
-	const CourseResult getGrade(const std::vector<double>& grades) const;
+    const CourseResult getGrade(const std::vector<double>& grades) const;
 };
