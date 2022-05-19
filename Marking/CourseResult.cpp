4632a4081c0d730479fdf8e0c2f81b8f8ba4e3b7
Connor-Colenso <52056774+Connor-Colenso@users.noreply.github.com>
2022-05-19 16:08:44 +0100

    Update CourseResult.cpp

--- Marking/CourseResult.cpp
+++ Marking/CourseResult.cpp
@@ -2,21 +2,20 @@
 #include <plog/Log.h>
 
 CourseResult::CourseResult(const double& tmp_score, const bool& result) : result(result) {
-	
-	// Tiny bit of room for floating point errors.
-	if ((tmp_score >= 0 - 1e-8) && (tmp_score <= 100 + 1e-8)) {
-		score = tmp_score;
-	} else {
-		plog::init(plog::info, "Log.txt");
-		LOG(plog::fatal) << "Score provided to CourseResult constructor for is out of range. Score provided was " + std::to_string(tmp_score) + ".";
-		throw std::invalid_argument("Score provided to CourseResult constructor is out of range. See log for details.");
-	}
+    // Tiny bit of room for floating point errors.
+    if ((tmp_score >= 0 - 1e-8) && (tmp_score <= 100 + 1e-8)) {
+        score = tmp_score;
+    } else {
+        plog::init(plog::info, "Log.txt");
+        LOG(plog::fatal) << "Score provided to CourseResult constructor for is out of range. Score provided was " + std::to_string(tmp_score) + ".";
+        throw std::invalid_argument("Score provided to CourseResult constructor is out of range. See log for details.");
+    }
 }
 
 const bool CourseResult::getResult() const {
-	return result;
+    return result;
 }
 
 const double CourseResult::getScore() const {
-	return score;
+    return score;
 }
