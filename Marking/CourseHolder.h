21f5d55d7b5eb558cf29144879603cc24b6a1ac6
Connor-Colenso <52056774+Connor-Colenso@users.noreply.github.com>
2022-05-19 16:07:58 +0100

    Update CourseHolder.h

--- Marking/CourseHolder.h
+++ Marking/CourseHolder.h
@@ -1,28 +1,26 @@
 #pragma once
+#include "Course.h"
+#include <plog/Log.h>
 #include <map>
 #include <string>
 #include <memory>
-#include <plog/Log.h>
-#include "Course.h"
 
 const enum CourseType {
-	ExamOnlyCourse = 0,
-	CourseworkOnlyCourse = 1,
-	HybridCourse = 2
+    ExamOnlyCourse = 0,
+    CourseworkOnlyCourse = 1,
+    HybridCourse = 2
 };
 
 const class CourseHolder {
 private:
-	std::map<std::string, std::shared_ptr<Course>> map_id_to_course;
-	std::map<std::string, std::string> map_id_to_name;
-	std::map<std::string, Credits> map_id_to_credits;
+    std::map<std::string, std::shared_ptr<Course>> map_id_to_course;
+    std::map<std::string, std::string> map_id_to_name;
+    std::map<std::string, Credits> map_id_to_credits;
 public:
-	//CourseHolder(std::vector<Course*> courses);
-	CourseHolder(const std::string& file_path_name);
-	CourseHolder();
+    CourseHolder(const std::string& file_path_name);
+    CourseHolder();
 
-	const std::shared_ptr<Course> getCourse(const std::string& course_code) const;
-	const std::map<std::string, std::shared_ptr<Course>> getCourseMap() const;
-	const std::string getCourseName(const std::string& course_code) const;
+    const std::shared_ptr<Course> getCourse(const std::string& course_code) const;
+    const std::map<std::string, std::shared_ptr<Course>> getCourseMap() const;
+    const std::string getCourseName(const std::string& course_code) const;
 };
-
