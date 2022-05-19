24fd649da2ac37aa72d994f65d72149220982f19
Connor-Colenso <52056774+Connor-Colenso@users.noreply.github.com>
2022-05-19 16:20:35 +0100

    Update StudentHolder.h

--- Marking/StudentHolder.h
+++ Marking/StudentHolder.h
@@ -1,34 +1,33 @@
 #pragma once
+#include <windows.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <plog/Log.h>
 #include <map>
 #include <string>
 #include <memory>
 #include <locale>
 #include <iomanip>
-#include <windows.h>
-#include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <set>
 #include <cstdlib>
 #include <codecvt>
 #include "Course.h"
 #include "Student.h"
-#include <plog/Log.h>
 
 const class StudentHolder {
 private:
-	// Internal variables.
-	std::vector<Student> students;
-	std::map<std::string, Student> map_id_to_student;
+    // Internal variables.
+    std::vector<Student> students;
+    std::map<std::string, Student> map_id_to_student;
 public:
-	// Constructor.
-	StudentHolder(const std::string& file_path_name);
-	StudentHolder();
+    // Constructor.
+    StudentHolder(const std::string& file_path_name);
+    StudentHolder();
 
-	// Methods.
-	const Student getStudent(const std::string& student_id) const;
-	const std::vector<double> getGrades(const std::string& student_id, const std::string& course_id) const;
-	const std::map<std::string, Student> getStudentMap() const;
-	void niceOutput(const std::string& student_id, const CourseHolder& courses) const;
+    // Methods.
+    const Student getStudent(const std::string& student_id) const;
+    const std::vector<double> getGrades(const std::string& student_id, const std::string& course_id) const;
+    const std::map<std::string, Student> getStudentMap() const;
+    void niceOutput(const std::string& student_id, const CourseHolder& courses) const;
 };
-
